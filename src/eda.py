53c2774e6db3d18aac9759e89c2ee63238969799
Yusuf-Youssef <yusuf.youssef2000@gmail.com>
2024-08-02 19:01:54 +0400

    Update eda.py

--- src/eda.py
+++ src/eda.py
@@ -1,1 +1,20 @@
+import pandas as pd
+
+# Load preprocessed data
+preprocessed_online_retail_path = '../data/preprocessed_online_retail.csv'
+preprocessed_amazon_reviews_path = '../data/preprocessed_amazon_reviews.csv'
+
+online_retail_df = pd.read_csv(preprocessed_online_retail_path)
+amazon_reviews_df = pd.read_csv(preprocessed_amazon_reviews_path)
+
+# Display basic information and statistics
+print("Online Retail Dataset Info:")
+print(online_retail_df.info())
+print("\nOnline Retail Dataset Description:")
+print(online_retail_df.describe())
+
+print("\nAmazon Reviews Dataset Info:")
+print(amazon_reviews_df.info())
+print("\nAmazon Reviews Dataset Description:")
+print(amazon_reviews_df.describe())
 
